ff7f943ec60279669637735a50f4bb22ecf9cac8
priyanka <priyanka@peerbits.com>
2018-11-02 17:07:18 +0530

    first commit

---
+++ PodDemoTest/PodDemoTest.h
@@ -0,0 +1,19 @@
+//
+//  PodDemoTest.h
+//  PodDemoTest
+//
+//  Created by Peerbits 8 on 02/11/18.
+//  Copyright © 2018 Peerbits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodDemoTest.
+FOUNDATION_EXPORT double PodDemoTestVersionNumber;
+
+//! Project version string for PodDemoTest.
+FOUNDATION_EXPORT const unsigned char PodDemoTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodDemoTest/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'PodDemoTest' do
+    pod 'Alamofire', '~> 4.7'
+    pod 'MBProgressHUD', '~> 1.1.0', :modular_headers => true
+end
+
